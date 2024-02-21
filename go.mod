13139c077b7efbe26285c8d396660249b52dc8ed
Sascha Grunert <sgrunert@redhat.com>
2024-02-21 14:53:08 +0100

    Init
    Signed-off-by: Sascha Grunert <sgrunert@redhat.com>

---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/saschagrunert/seccomp-oci-artifact-demo
+
+go 1.22.0
+
+require github.com/saschagrunert/demo v0.0.0-20240129082513-facba5090fd7
+
+require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/gookit/color v1.5.4 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/urfave/cli/v2 v2.27.1 // indirect
+	github.com/xo/terminfo v0.0.0-20210125001918-ca9a967f8778 // indirect
+	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
+	golang.org/x/sys v0.15.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	. "github.com/saschagrunert/demo"
+)
+
+func main() {
+	d := New()
+	d.Add(demo(), "run", "Run the demo")
+	d.Run()
+}
+
+func demo() *Run {
+	r := NewRun(
+		"CRI-O - seccomp profiles as OCI artifacts",
+		"This demo outlines how to utilize seccomp",
+		"profiles as OCI artifacts in the upcoming",
+		"CRI-O v1.30.0 release.",
+	)
+
+	r.Step(S(
+		"CRI-O has to be configured to allow the",
+		"annotation `seccomp-profile.kubernetes.cri-o.io`",
+		"for the used runtime",
+	), S(
+		"cat 10-runtimes.conf",
+	))
+
+	r.Step(S(
+		"To apply a seccomp profile to a whole pod",
+		"use the annotation including the suffix `/POD",
+	), S(
+		"cat pod.yml",
+	))
+
+	r.Step(nil, S("kubectl apply -f pod.yml"))
+
+	r.Step(S(
+		"The pod should now run using the profile, rather",
+		"than running `Unconfined`",
+	), S(
+		"export CONTAINER_ID=$(sudo crictl ps --name container -q) &&",
+		"sudo crictl inspect $CONTAINER_ID | jq .info.runtimeSpec.linux.seccomp | head",
+	))
+
+	r.Step(nil, S("kubectl delete -f pod.yml"))
+
+	r.Step(S(
+		"It's also possible to apply the profile to a specific container",
+		"by using the `/<CONTAINER_NAME>` suffix at the anntation level",
+	), S(
+		"cat pod-ctr.yml",
+	))
+
+	r.Step(nil, S("kubectl apply -f pod-ctr.yml"))
+
+	r.Step(S(
+		"Again, the container should now run using the profile, rather",
+		"than running `Unconfined`",
+	), S(
+		"export CONTAINER_ID=$(sudo crictl ps --name container -q) &&",
+		"sudo crictl inspect $CONTAINER_ID | jq .info.runtimeSpec.linux.seccomp | head",
+	))
+
+	r.Step(nil, S("kubectl delete -f pod-ctr.yml"))
+
+	r.Step(S(
+		"It's also possible to link container images to seccomp profiles.",
+		"For that, the image has to be built using the annotaion",
+		"seccomp-profile.kubernetes.cri-o.io=<IMAGE>",
+		"",
+		"For example, the image quay.io/crio/nginx-seccomp:generic",
+		"has been created that way",
+	), S(
+		"skopeo inspect --raw docker://quay.io/crio/nginx-seccomp:generic |",
+		"jq .annotations",
+	))
+
+	r.Step(S(
+		"If I now use that image in an pod definition",
+	), S(
+		"cat pod-img.yml",
+	))
+
+	r.Step(nil, S("kubectl apply -f pod-img.yml"))
+
+	r.Step(S(
+		"Then, the pod should now run using the profile, too",
+	), S(
+		"export CONTAINER_ID=$(sudo crictl ps --name container -q) &&",
+		"sudo crictl inspect $CONTAINER_ID | jq .info.runtimeSpec.linux.seccomp | head",
+	))
+
+	r.Step(nil, S("kubectl delete -f pod.yml"))
+
+	r.Step(S(
+		"Image annotations also work using the `/POD` or `/<CONTAINER>` suffix.",
+		"",
+		"The seccomp profile itself consists of a single layer",
+		"containing the seccomp.json file in a tar archive.",
+	), S(
+		"skopeo inspect --raw docker://quay.io/crio/seccomp:v1 | jq .",
+	))
+
+	r.Step(S(
+		"This allows managing the whole seccomp profile in containers/storage",
+		"as well as stacking profiles together (future work).",
+		"",
+		"Pushing profiles can be done using ORAS: https://oras.land",
+	), nil)
+
+	return r
+}
